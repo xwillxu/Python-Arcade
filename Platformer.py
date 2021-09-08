b108b5fe9f6c81e93d8b6c992bb2cb9ae5ac8537
xwillxu <hairblack088@gmail.com>
2021-09-08 10:13:46 -0400

    Platformer

--- Platformer.py
+++ Platformer.py
@@ -1,23 +1,51 @@
-# Platformer
-
+"""
+Example of Pymunk Physics Engine Platformer
+"""
 import arcade
 
-SCREEN_WIDTH = 1000
-SCREEN_HEIGHT = 650
-SCREEN_TITLE = 'Platformer'
+SCREEN_TITLE = "PyMunk Platformer"
+
+# Size of screen to show, in pixels
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
 
 
-class Game(arcade.Window):
-    """Game window"""
+class GameWindow(arcade.Window):
+    """ Main Window """
 
-    def __init__(self):
-        '''__init__'''
+    def __init__(self, width, height, title):
+        """ Create the variables """
 
-        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+        # Init the parent class
+        super().__init__(width, height, title)
 
-        self.left_pressed = False
-        self.right_pressed = False
-        self.up_pressed = False
-        self.down_pressed = False
-        self.shoot_pressed = False
-        self.jump_needs_reset = False
+    def setup(self):
+        """ Set up everything with the game """
+        pass
+
+    def on_key_press(self, key, modifiers):
+        """Called whenever a key is pressed. """
+        pass
+
+    def on_key_release(self, key, modifiers):
+        """Called when the user releases a key. """
+        pass
+
+    def on_update(self, delta_time):
+        """ Movement and game logic """
+        pass
+
+    def on_draw(self):
+        """ Draw everything """
+        arcade.start_render()
+
+
+def main():
+    """ Main method """
+    window = GameWindow(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+    window.setup()
+    arcade.run()
+
+
+if __name__ == "__main__":
+    main()
