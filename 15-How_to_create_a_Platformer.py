a633335c2e6c7b4ffcbc61100cee3dc8b955690e
xwillxu <hairblack088@gmail.com>
2021-11-19 17:14:34 -0500

    How to create a platformer 17

--- 15-How_to_create_a_Platformer.py
+++ 15-How_to_create_a_Platformer.py
@@ -9,7 +9,7 @@ import os
 SCREEN_WIDTH = 1200
 SCREEN_HEIGHT = 800
 SCREEN_TITLE = 'Platformer Tutorial'
-
+BULLET_SPEED = 4
 SCALE = 0.8
 
 # Create Class
@@ -23,6 +23,7 @@ class Game(arcade.Window):
         arcade.set_background_color(arcade.color.EMERALD)
 
         self.enemy_list = arcade.SpriteList()
+        self.bullet_list = arcade.SpriteList()
 
         file_path = os.path.dirname(os.path.abspath(__file__))
         os.chdir(file_path)
@@ -63,8 +64,6 @@ class Game(arcade.Window):
 
         self.enemy(0)
 
-        arcade.schedule(self.enemy, 2)
-
     def play_music(self, delta_time: float):
         arcade.play_sound(self.background_music)
 
@@ -91,11 +90,14 @@ class Game(arcade.Window):
         if key == arcade.key.LEFT:
             self.player.change_x = 0
 
-    def on_update(self, delta_time: float):
+    def on_update(self, delta_time):
         """On Update"""
 
         self.player.update()
         self.frame_count += 1
+        #print(f'frame count', self.frame_count)
+        print(f'enemy count', self.enemy_list.__len__())
+        print(f'bullet count', self.bullet_list.__len__())
 
         for enemy in self.enemy_list:
             start_x = enemy.center_x
@@ -109,6 +111,26 @@ class Game(arcade.Window):
             angle = math.atan2(y_diff, x_diff)
 
             enemy.angle = math.degrees(angle)-90
+
+            if self.frame_count % 60 == 0:
+                bullet = arcade.Sprite(
+                    ":resources:images/space_shooter/laserBlue01.png")
+                bullet.center_x = start_x
+                bullet.center_y = start_y
+
+                bullet.angle = math.degrees(angle)
+
+                bullet.change_x = math.cos(angle) * BULLET_SPEED
+                bullet.change_y = math.sin(angle) * BULLET_SPEED
+
+                self.bullet_list.append(bullet)
+
+        for bullet in self.bullet_list:
+            if bullet.top < 0 or bullet.top > SCREEN_HEIGHT or bullet.right > SCREEN_WIDTH or bullet.right < 0:
+                bullet.remove_from_sprite_lists()
+
+        self.enemy_list.update()
+        self.bullet_list.update()
 
         if self.player.top > self.height:
             self.player.top = self.height
@@ -125,6 +147,7 @@ class Game(arcade.Window):
         # Draw Sprites
         self.player.draw()
         self.enemy_list.draw()
+        self.bullet_list.draw()
 
 
 if __name__ == "__main__":
