1861a2b2f4f68ac416e6995ba6302df1688641d0
xwillxu <hairblack088@gmail.com>
2021-11-23 16:41:24 -0500

    How to create a slime invaders game 2

--- How_to_create_a_Slime_Invaders_game.py
+++ How_to_create_a_Slime_Invaders_game.py
@@ -15,3 +15,28 @@ class Game(arcade.Window):
     """Game Window"""
 
     def __init__(self):
+        """Init"""
+        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+
+        arcade.set_background_color(arcade.color.AMAZON)
+
+    def setup(self):
+        """Setup"""
+
+        pass
+
+    def on_update(self, delta_time: float):
+        """Update"""
+
+        pass
+
+    def on_draw(self):
+        """Draw window"""
+
+        arcade.start_render()
+
+
+if __name__ == "__main__":
+    window = Game()
+    window.setup()
+    arcade.run()
