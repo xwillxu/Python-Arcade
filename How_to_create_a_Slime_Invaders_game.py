6813adf94a83d6bb2136bcf750f6b75098eb6b1b
xwillxu <hairblack088@gmail.com>
2021-11-23 16:36:59 -0500

    How to create a slime invaders game 1

---
+++ How_to_create_a_Slime_Invaders_game.py
@@ -0,0 +1,17 @@
+# Slime Invaders
+
+# Imports
+import arcade
+
+# Screen varibles
+SCREEN_WIDTH = 1200
+SCREEN_HEIGHT = 800
+SCREEN_TITLE = "Slime Invaders"
+
+# Classes
+
+
+class Game(arcade.Window):
+    """Game Window"""
+
+    def __init__(self):
