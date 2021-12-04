0d6723ded68c76ed442b4d939277dd108b3d43c2
xwillxu <hairblack088@gmail.com>
2021-12-04 11:46:56 -0500

    How to create a scrolling platformer 23

--- 16-Extension_How_to_create_a_scrolling_Platformer.py
+++ 16-Extension_How_to_create_a_scrolling_Platformer.py
@@ -20,6 +20,10 @@ PLAYER_MOVEMENT_SPEED = 10
 GRAVITY = 1
 PLAYER_JUMP_SPEED = 20
 
+SPRITE_SCALING = 0.5
+SPRITE_NATIVE_SIZE = 128
+SPRITE_SIZE = 64
+
 
 class MyGame(arcade.Window):
     """
@@ -212,17 +216,17 @@ class MyGame(arcade.Window):
 
         self.enemy_list.update()
 
-          # Check each enemy
-          for enemy in self.enemy_list:
-               # If the enemy hit a wall, reverse
-               if len(arcade.check_for_collision_with_list(enemy, self.wall_list)) > 0:
-                    enemy.change_x *= -1
-                # If the enemy hit the left boundary, reverse
-                elif enemy.boundary_left is not None and enemy.left < enemy.boundary_left:
-                    enemy.change_x *= -1
-                # If the enemy hit the right boundary, reverse
-                elif enemy.boundary_right is not None and enemy.right > enemy.boundary_right:
-                    enemy.change_x *= -1
+        # Check each enemy
+        for enemy in self.enemy_list:
+            # If the enemy hit a wall, reverse
+            if len(arcade.check_for_collision_with_list(enemy, self.wall_list)) > 0:
+                enemy.change_x *= -1
+            # If the enemy hit the left boundary, reverse
+            elif enemy.boundary_left is not None and enemy.left < enemy.boundary_left:
+                enemy.change_x *= -1
+            # If the enemy hit the right boundary, reverse
+            elif enemy.boundary_right is not None and enemy.right > enemy.boundary_right:
+                enemy.change_x *= -1
 
         # See if we hit any coins
         coin_hit_list = arcade.check_for_collision_with_list(
