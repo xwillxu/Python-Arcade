ac4dda19fd3ff21e0c74a9bd53cf02471c156008
xwillxu <hairblack088@gmail.com>
2021-12-06 16:19:59 -0500

    How to create a scrolling platformer 46

--- 16-Extension_How_to_create_a_scrolling_Platformer.py
+++ 16-Extension_How_to_create_a_scrolling_Platformer.py
@@ -195,6 +195,45 @@ class MyGame(arcade.Window):
 
             self.enemy_list.append(bee)
 
+    def bullet(self):
+        """Bullet"""
+
+        # Create a bullet
+        bullet = arcade.Sprite(
+            "RealPython/materials/arcade-a-primer/images/missile.png", 2, 0, 0, 0, 0, 0, 0, 1, 1, True)
+
+        # Position the bullet at the player's current location
+        start_x = self.player_sprite.center_x
+        start_y = self.player_sprite.center_y
+        bullet.center_x = start_x
+        bullet.center_y = start_y
+
+        # Get from the mouse the destination location for the bullet
+        # IMPORTANT! If you have a scrolling screen, you will also need
+        # to add in self.view_bottom and self.view_left.
+        dest_x = x
+        dest_y = y
+
+        # Do math to calculate how to get the bullet to the destination.
+        # Calculation the angle in radians between the start points
+        # and end points. This is the angle the bullet will travel.
+        x_diff = dest_x - start_x
+        y_diff = dest_y - start_y
+        angle = math.atan2(y_diff, x_diff)
+
+        # Angle the bullet sprite so it doesn't look like it is flying
+        # sideways.
+        bullet.angle = math.degrees(angle)
+        # print(f"Bullet angle: {bullet.angle:.2f}")
+
+        # Taking into account the angle, calculate our change_x
+        # and change_y. Velocity is how fast the bullet travels.
+        bullet.change_x = math.cos(angle) * BULLET_SPEED
+        bullet.change_y = math.sin(angle) * BULLET_SPEED
+
+        # Add the bullet to the appropriate lists
+        self.bullet_list.append(bullet)
+
     def on_draw(self):
         """Render the screen."""
 
@@ -246,41 +285,7 @@ class MyGame(arcade.Window):
     def on_mouse_press(self, x, y, button, modifiers):
         """ Called whenever the mouse button is clicked. """
 
-        # Create a bullet
-        bullet = arcade.Sprite(
-            "RealPython/materials/arcade-a-primer/images/missile.png", 2, 0, 0, 0, 0, 0, 0, 1, 1, True)
-
-        # Position the bullet at the player's current location
-        start_x = self.player_sprite.center_x
-        start_y = self.player_sprite.center_y
-        bullet.center_x = start_x
-        bullet.center_y = start_y
-
-        # Get from the mouse the destination location for the bullet
-        # IMPORTANT! If you have a scrolling screen, you will also need
-        # to add in self.view_bottom and self.view_left.
-        dest_x = x
-        dest_y = y
-
-        # Do math to calculate how to get the bullet to the destination.
-        # Calculation the angle in radians between the start points
-        # and end points. This is the angle the bullet will travel.
-        x_diff = dest_x - start_x
-        y_diff = dest_y - start_y
-        angle = math.atan2(y_diff, x_diff)
-
-        # Angle the bullet sprite so it doesn't look like it is flying
-        # sideways.
-        bullet.angle = math.degrees(angle)
-        print(f"Bullet angle: {bullet.angle:.2f}")
-
-        # Taking into account the angle, calculate our change_x
-        # and change_y. Velocity is how fast the bullet travels.
-        bullet.change_x = math.cos(angle) * BULLET_SPEED
-        bullet.change_y = math.sin(angle) * BULLET_SPEED
-
-        # Add the bullet to the appropriate lists
-        self.bullet_list.append(bullet)
+        self.bullet()
 
     def center_camera_to_player(self):
         screen_center_x = self.player_sprite.center_x - \
@@ -319,6 +324,11 @@ class MyGame(arcade.Window):
 
         for sprite in self.enemy_list:
             if self.player_sprite.collides_with_sprite(sprite):
+                sprite.remove_from_sprite_lists()
+                arcade.close_window()
+
+        for sprite in self.enemy_list:
+            if bullet.collides_with_sprite(sprite):
                 sprite.remove_from_sprite_lists()
                 arcade.close_window()
 
