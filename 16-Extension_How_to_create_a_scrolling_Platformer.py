43015a3a024699f95f1cbab38002d3241c7d28f0
xwillxu <hairblack088@gmail.com>
2021-12-06 16:13:47 -0500

    How to create a scrolling platformer 44

--- 16-Extension_How_to_create_a_scrolling_Platformer.py
+++ 16-Extension_How_to_create_a_scrolling_Platformer.py
@@ -2,6 +2,8 @@
 Platformer Game
 """
 import arcade
+import math
+import os
 
 # Constants
 SCREEN_WIDTH = 1200
@@ -21,6 +23,8 @@ GRAVITY = 1
 PLAYER_JUMP_SPEED = 20
 SPRITE_SIZE = 64
 
+BULLET_SPEED = 5
+
 
 class MyGame(arcade.Window):
     """
@@ -31,6 +35,9 @@ class MyGame(arcade.Window):
 
         # Call the parent class and set up the window
         super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+
+        file_path = os.path.dirname(os.path.abspath(__file__))
+        os.chdir(file_path)
 
         # Our TileMap Object
         self.tile_map = None
@@ -187,11 +194,6 @@ class MyGame(arcade.Window):
 
             self.enemy_list.append(bee)
 
-    def bullet(self):
-        """Bullet"""
-
-        bullet = arcade.Sprite("")
-
     def on_draw(self):
         """Render the screen."""
 
@@ -238,6 +240,45 @@ class MyGame(arcade.Window):
             self.player_sprite.change_x = 0
         elif key == arcade.key.RIGHT or key == arcade.key.D:
             self.player_sprite.change_x = 0
+
+    def on_mouse_press(self, x, y, button, modifiers):
+        """ Called whenever the mouse button is clicked. """
+
+        # Create a bullet
+        bullet = arcade.Sprite(
+            "RealPython/materials/arcade-a-primer/images/missile.png", 1)
+
+        # Position the bullet at the player's current location
+        start_x = self.player_sprite.center_x
+        start_y = self.player_sprite.center_y
+        bullet.center_x = start_x
+        bullet.center_y = start_y
+
+        # Get from the mouse the destination location for the bullet
+        # IMPORTANT! If you have a scrolling screen, you will also need
+        # to add in self.view_bottom and self.view_left.
+        dest_x = x
+        dest_y = y
+
+        # Do math to calculate how to get the bullet to the destination.
+        # Calculation the angle in radians between the start points
+        # and end points. This is the angle the bullet will travel.
+        x_diff = dest_x - start_x
+        y_diff = dest_y - start_y
+        angle = math.atan2(y_diff, x_diff)
+
+        # Angle the bullet sprite so it doesn't look like it is flying
+        # sideways.
+        bullet.angle = math.degrees(angle)
+        print(f"Bullet angle: {bullet.angle:.2f}")
+
+        # Taking into account the angle, calculate our change_x
+        # and change_y. Velocity is how fast the bullet travels.
+        bullet.change_x = math.cos(angle) * BULLET_SPEED
+        bullet.change_y = math.sin(angle) * BULLET_SPEED
+
+        # Add the bullet to the appropriate lists
+        self.bullet_list.append(bullet)
 
     def center_camera_to_player(self):
         screen_center_x = self.player_sprite.center_x - \
