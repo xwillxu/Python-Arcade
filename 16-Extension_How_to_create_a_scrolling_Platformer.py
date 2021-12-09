fd319a2af732bc19063a8f0f974657d5f6945458
xwillxu <hairblack088@gmail.com>
2021-12-09 16:12:58 -0500

    Add Health bar

--- 16-Extension_How_to_create_a_scrolling_Platformer.py
+++ 16-Extension_How_to_create_a_scrolling_Platformer.py
@@ -25,6 +25,37 @@ SPRITE_SIZE = 64
 
 BULLET_SPEED = 30
 
+# HealthBar Setup
+HEALTHBAR_WIDTH = 50
+HEALTHBAR_HEIGHT = 6
+HEALTHBAR_OFFSET_Y = -10
+
+
+class Health_Sprite(arcade.Sprite):
+    '''Health Sprite'''
+
+    def __init__(self, image, scale, max_health):
+        super().__init__(image, scale)
+
+        self.max_health = max_health
+        self.cur_health = max_health
+
+    def draw_health_bar(self):
+        if self.cur_health < self.max_health:
+            arcade.draw_rectangle_filled(center_x=self.center_x,
+                                         center_y=self.center_y + HEALTHBAR_OFFSET_Y,
+                                         width=HEALTHBAR_WIDTH,
+                                         height=5,
+                                         color=arcade.color.RED)
+
+        health_width = HEALTHBAR_WIDTH * (self.cur_health / self.max_health)
+
+        arcade.draw_rectangle_filled(center_x=self.center_x - 0.5 * (HEALTHBAR_WIDTH - health_width),
+                                     center_y=self.center_y - 10,
+                                     width=health_width,
+                                     height=HEALTHBAR_HEIGHT,
+                                     color=arcade.color.GREEN)
+
 
 class MyGame(arcade.Window):
     """
