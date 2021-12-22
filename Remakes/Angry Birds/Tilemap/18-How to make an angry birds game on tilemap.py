1074315fa83b80afb20f7ddc9d6fbf2d53ce0237
xwillxu <hairblack088@gmail.com>
2021-12-22 16:55:16 -0500

    Add physics engine

--- Remakes/Angry Birds/Tilemap/18-How to make an angry birds game on tilemap.py
+++ Remakes/Angry Birds/Tilemap/18-How to make an angry birds game on tilemap.py
@@ -24,9 +24,21 @@ SCREEN_WIDTH = 1200
 SCREEN_HEIGHT = 800
 SCREEN_TITLE = "Angry Birds"
 
+# Scale
 SCALE = 0.5
+
+# Bullet Speed
 BULLET_SPEED = 30
-TILE_SCALING = 0.5
+
+# Gravity
+GRAVITY = 1500
+
+# Damping - Amount of speed lost per second
+DEFAULT_DAMPING = 1.0
+
+# Friction between objects
+WALL_FRICTION = 0.7
+DYNAMIC_ITEM_FRICTION = 0.6
 
 
 class Game(arcade.Window):
@@ -37,14 +49,71 @@ class Game(arcade.Window):
 
         super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
 
-        pass
+        # Our TileMap Object
+        self.tile_map = None
+
+        # Our Scene Object
+        self.scene = None
+
+        # Our physics enginea
+        self.physics_engine = None
+
+        # Lists
+        self.wall_list = None
+        self.item_list = None
 
     def setup(self):
         """Setup"""
 
         arcade.set_background_color(arcade.color.BLUE_YONDER)
 
-        pass
+        # Map name
+        map_name = "arcade/doc/tutorials/pymunk_platformer/pymunk_test_map.json"
+
+        # Load in TileMap
+        tile_map = arcade.load_tilemap(map_name, SCALE)
+
+        # Pull the sprite layers out of the tile map
+        self.wall_list = tile_map.sprite_lists["Platform"]
+        self.item_list = tile_map.sprite_lists["Boxes"]
+
+        # --- Pymunk Physics Engine Setup ---
+
+        # The default damping for every object controls the percent of velocity
+        # the object will keep each second. A value of 1.0 is no speed loss,
+        # 0.9 is 10% per second, 0.1 is 90% per second.
+        # For top-down games, this is basically the friction for moving objects.
+        # For platformers with gravity, this should probably be set to 1.0.
+        # Default value is 1.0 if not specified.
+        damping = DEFAULT_DAMPING
+
+        # Set the gravity. (0, 0) is good for outer space and top-down.
+        gravity = (0, -GRAVITY)
+
+        # Create the physics engine
+        self.physics_engine = arcade.PymunkPhysicsEngine(damping=damping,
+                                                         gravity=gravity)
+
+        # Create the walls.
+        # By setting the body type to PymunkPhysicsEngine.STATIC the walls can't
+        # move.
+        # Movable objects that respond to forces are PymunkPhysicsEngine.DYNAMIC
+        # PymunkPhysicsEngine.KINEMATIC objects will move, but are assumed to be
+        # repositioned by code and don't respond to physics forces.
+        # Dynamic is default.
+        self.physics_engine.add_sprite_list(self.wall_list,
+                                            friction=WALL_FRICTION,
+                                            collision_type="wall",
+                                            body_type=arcade.PymunkPhysicsEngine.STATIC)
+
+        # Create the items
+        self.physics_engine.add_sprite_list(self.item_list,
+                                            friction=DYNAMIC_ITEM_FRICTION,
+                                            collision_type="item")
+
+        # Add kinematic sprites
+        self.physics_engine.add_sprite_list(self.moving_sprites_list,
+                                            body_type=arcade.PymunkPhysicsEngine.KINEMATIC)
 
     def on_update(self, delta_time):
         """Update"""
