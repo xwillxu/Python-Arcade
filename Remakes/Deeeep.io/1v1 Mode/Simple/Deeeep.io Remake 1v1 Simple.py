55aa201bee2208d5b68569977563c4c8d84c5e15
xwillxu <hairblack088@gmail.com>
2022-01-23 18:04:27 -0500

    Add Health Bar

--- Remakes/Deeeep.io/1v1 Mode/Simple/Deeeep.io Remake 1v1 Simple.py
+++ Remakes/Deeeep.io/1v1 Mode/Simple/Deeeep.io Remake 1v1 Simple.py
@@ -22,6 +22,37 @@ TINY_SCALE = 0.7
 SCALE = 0.4
 SUPER_SCALE = 0.2
 
+# HealthBar Setup
+HEALTHBAR_WIDTH = 50
+HEALTHBAR_HEIGHT = 10
+HEALTHBAR_OFFSET_Y = 50
+
+
+class Health_Sprite(arcade.Sprite):
+    '''Health Sprite'''
+
+    def __init__(self, image, scale, max_health):
+        super().__init__(image, scale)
+
+        self.max_health = max_health
+        self.cur_health = max_health
+
+    def draw_health_bar(self):
+        if self.cur_health < self.max_health:
+            arcade.draw_rectangle_filled(center_x=self.center_x,
+                                         center_y=self.center_y + HEALTHBAR_OFFSET_Y,
+                                         width=HEALTHBAR_WIDTH,
+                                         height=HEALTHBAR_HEIGHT,
+                                         color=arcade.color.RED)
+
+        health_width = HEALTHBAR_WIDTH * (self.cur_health / self.max_health)
+
+        arcade.draw_rectangle_filled(center_x=self.center_x - 0.5 * (HEALTHBAR_WIDTH - health_width),
+                                     center_y=self.center_y + HEALTHBAR_OFFSET_Y,
+                                     width=health_width,
+                                     height=HEALTHBAR_HEIGHT,
+                                     color=arcade.color.GREEN)
+
 
 class Game(arcade.Window):
     """Game"""
