f20b31cbbfb5f373049da083a26feab77931be38
xwillxu <hairblack088@gmail.com>
2022-02-27 11:45:06 -0500

    Add Health Sprites

--- Remakes/Deeeep.io/Using Pymunk/Deeeep.io-Remake-Using-Pymunk.py
+++ Remakes/Deeeep.io/Using Pymunk/Deeeep.io-Remake-Using-Pymunk.py
@@ -16,6 +16,11 @@ SCREEN_WIDTH = 1200
 SCREEN_HEIGHT = 800
 SCREEN_TITLE = "Deeeep.io(Remake) Using Pymunk"
 
+# HealthBar Setup
+HEALTHBAR_WIDTH = 50
+HEALTHBAR_HEIGHT = 10
+HEALTHBAR_OFFSET_Y = 50
+
 # Animal Dictionary
 animal_name_list = [
     'Alligator_Snapping_Turtle',
@@ -153,6 +158,32 @@ animals = {
 }
 
 # Classes
+
+
+class Health_Sprite(arcade.Sprite):
+    '''Health Sprite'''
+
+    def __init__(self, image, scale, max_health):
+        super().__init__(image, scale)
+
+        self.max_health = max_health
+        self.cur_health = max_health
+
+    def draw_health_bar(self):
+        if self.cur_health < self.max_health:
+            arcade.draw_rectangle_filled(center_x=self.center_x,
+                                         center_y=self.center_y + HEALTHBAR_OFFSET_Y,
+                                         width=HEALTHBAR_WIDTH,
+                                         height=HEALTHBAR_HEIGHT,
+                                         color=arcade.color.BLACK)
+
+        health_width = HEALTHBAR_WIDTH * (self.cur_health / self.max_health)
+
+        arcade.draw_rectangle_filled(center_x=self.center_x - 0.5 * (HEALTHBAR_WIDTH - health_width),
+                                     center_y=self.center_y + HEALTHBAR_OFFSET_Y,
+                                     width=health_width,
+                                     height=HEALTHBAR_HEIGHT,
+                                     color=arcade.color.GREEN)
 
 
 class PhysicsSprite(arcade.Sprite):
